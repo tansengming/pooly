98d5dd7b2f7a377b3f3423c1764374289c2ae065
SengMing Tan <tansengming@gmail.com>
2016-11-30 11:55:53 -0500

    test cleanup
    thanks for the suggestion @margold

--- test/sample_worker_test.exs
+++ test/sample_worker_test.exs
@@ -1,14 +1,15 @@
 defmodule SampleWorkerTest do
   use ExUnit.Case
+  import SampleWorker, only: [start_link: 1]
 
   test 'start_link' do
-    {:ok, pid} = SampleWorker.start_link([])
+    {:ok, pid} = start_link([])
 
     assert pid != nil
   end
 
   test 'stop' do
-    {:ok, pid} = SampleWorker.start_link([])
+    {:ok, pid} = start_link([])
 
     SampleWorker.stop(pid)
     assert Process.alive?(pid) == false
